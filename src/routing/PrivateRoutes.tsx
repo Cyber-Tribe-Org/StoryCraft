2e89cbb1b8291e9ea968b703377e1e4b34736217
Dobromir Kovachev <dobromir.mail@gmail.com>
2024-05-24 14:34:54 +0300

    Fix the access to private pages

--- src/routing/PrivateRoutes.tsx
+++ src/routing/PrivateRoutes.tsx
@@ -4,18 +4,18 @@ import NavBar from "../components/NavBar";
 import { useState, useEffect } from "react";
 
 const PrivateRoutes = () => {
-    const { userEmail, isAuthenticated } = useAuth();
+    const { userEmail, isVerified } = useAuth();
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         setLoading(false);
-    }, [isAuthenticated, userEmail]);
+    }, [isVerified, userEmail]);
 
     if (loading) {
         return null;
     }
 
-    if (!isAuthenticated && userEmail) {
+    if (!isVerified && userEmail) {
         return <Navigate to="/login" />;
     }
 
